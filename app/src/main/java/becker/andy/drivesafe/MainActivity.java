dd67a84c3b340388bfbf82ae234c8503a833ea54
Md-Ali-Azad <aliazad969@gmail.com>
2018-12-10 17:35:16 +0600

    Merge remote-tracking branch 'remotes/origin/master' into splash
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/becker/andy/drivesafe/MainActivity.java
+++ app/src/main/java/becker/andy/drivesafe/MainActivity.java
@@ -1,6 +1,10 @@
 package becker.andy.drivesafe;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +13,10 @@ import android.view.MenuItem;
 import android.view.ViewConfiguration;
 
 import java.lang.reflect.Field;
+
+import becker.andy.drivesafe.fragments.generalItemOneFragment;
+import becker.andy.drivesafe.fragments.generalItemThreeFragment;
+import becker.andy.drivesafe.fragments.generalItemTwoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +35,32 @@ public class MainActivity extends AppCompatActivity {
         {
 
         }
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.general_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.driver_list:
+                                selectedFragment = generalItemOneFragment.newInstance();
+                                break;
+                            case R.id.recent_affairs:
+                                selectedFragment = generalItemTwoFragment.newInstance();
+                                break;
+                            case R.id.dang_driver:
+                                selectedFragment = generalItemThreeFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
