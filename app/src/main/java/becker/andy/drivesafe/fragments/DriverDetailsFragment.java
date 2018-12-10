70fdfbd974a8c7d0b8963631d04622baebd7b113
MD. Amzad Hossain Arif <arifamzad189@gmail.com>
2018-12-11 04:12:44 +0600

    commit from arif -1

--- app/src/main/java/becker/andy/drivesafe/fragments/DriverDetailsFragment.java
+++ app/src/main/java/becker/andy/drivesafe/fragments/DriverDetailsFragment.java
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import becker.andy.drivesafe.R;
 
@@ -14,6 +16,8 @@ import becker.andy.drivesafe.R;
  */
 public class DriverDetailsFragment extends Fragment {
 
+    TextView dName, dEmail, dPhone, dOwnerPhone, dLicense;
+    EditText cardPlate;
 
     public DriverDetailsFragment() {
         // Required empty public constructor
@@ -24,7 +28,17 @@ public class DriverDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_details, container, false);
+        View view= inflater.inflate(R.layout.fragment_driver_details, container, false);
+
+        dName = view.findViewById(R.id.name);
+        dEmail = view.findViewById(R.id.email);
+        dPhone = view.findViewById(R.id.phone);
+        dOwnerPhone = view.findViewById(R.id.ownerPhone);
+        dLicense = view.findViewById(R.id.license);
+
+        cardPlate = view.findViewById(R.id.numberPlate);
+
+        return view;
     }
 
 }
